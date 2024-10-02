ec03c26a6b5db7898d28eee2336496ba45a9f856
Bolo New <121732191+Bol-o@users.noreply.github.com>
2024-10-02 17:55:21 +0100

    REally pleased with me self bruv innit
    Feeling shit, but made myself go through this (fookin depresssian),
    I've set up a add and an edit button, using name state variable with
    both buttons. Am pleases as 15min ago was thinking 'I will never be able
    to do this', and little by little I did it!

--- runy-nose-react-app/src/persDetails.jsx
+++ runy-nose-react-app/src/persDetails.jsx
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css'
+
+//const [submButt, setSubmButt] = useState('')
+
+function submitButton(){
+  var submBut = document.getElementById('edit');
+  submBut.innerText = 'Edit'
+  
+} 
 
 
 function PersDetails(){
+
+  const [name, setName] = useState('');
+
+  const onButtonClick = () => {
+    const firstName = document.getElementById('firstName')
+
+    setName(firstName.value)
+    firstName.value = '';
+
+   
+    submitButton()
+  }
+
+  const editName = () => {
+
+    firstName.value = name
+    setName('')
+
+
+  }
+
+
 
     return (
         <>
@@ -12,8 +42,24 @@ function PersDetails(){
 
     <form>
       <label>Enter your first name:
-        <input type="text" />
+        <textarea type="text" id="firstName"> </textarea>
+        <button
+            type="button"
+            onClick={onButtonClick}
+            >Add</button>
+
+        <button 
+            id="edit"
+            type="button"
+            onClick={editName}
+        
+           >
+            
+
+        </button>
       </label>
+      <div>REsults: {name}</div>
+     
     </form>
     <form>
       <label>Enter your second name:
@@ -25,7 +71,10 @@ function PersDetails(){
         <input type="email" />
       </label>
     </form>
+
+    
     </div>
+    <button id="submit">Submit</button>
     </>
     )
 
