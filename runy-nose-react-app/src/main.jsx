359991412a1490495738620be8708e1556ee8051
Bolo New <121732191+Bol-o@users.noreply.github.com>
2024-10-03 16:55:28 +0100

    adding worki hist component
    Learning how to do it steb by step and not just copy pasting previous
    component and modifying. Names of main functions first letter capital,
    same when you paste it as am 'element' into the main.jsx.

--- runy-nose-react-app/src/main.jsx
+++ runy-nose-react-app/src/main.jsx
@@ -4,11 +4,13 @@ import App from './App.jsx'
 import './index.css'
 import PersDetails from './persDetails.jsx'
 import EduDetails from './education.jsx'
+import WorkHistory from './work-history.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <PersDetails />
     <EduDetails />
+    <WorkHistory />
   </StrictMode>,
 )
---
+++ runy-nose-react-app/src/work-history.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useState } from "react";
+import './App.css'
+
+function WorkHistory(){
+
+
+return(
+    <>
+
+
+    <div>
+    <h1>THIS IS WORK HISTORY</h1>
+    </div>
+
+    </>
+)
+
+}
+
+export default WorkHistory
